4a876251e18998304b28d125a706383ace2e347e
DurandSacha <sacha6623@gmail.com>
2020-10-28 00:11:04 +0100

    minor update ( comment line )

--- app/utils.py
+++ app/utils.py
@@ -10,6 +10,7 @@ def readExcelDocument(file):
 
 def drawDiagram():
     data = {
+        # line : id_function, Sequence, Operation Type, Description, Node1, Node2 
         "a":["1","1","Col","Lorem ipsum dolor sit amet, consectetur adipiscing elit","AX23","BY12"], 
         "b":["1","2","Tra","Donec fringilla, erat non suscipit faucibus, léo mi porta enim","BY12","CF43"],
         "c":["1","3","Tra","Proin quis tortor pharetra, pulvinar sem viate, lobortis","BY12","CG54"],
@@ -18,7 +19,6 @@ def drawDiagram():
         "f":["2","1","Col","Cras Suscipit hendrerit feugiat Viva porta sed consecta","AB56","KJ65"],
         "g":["2","2","Tra","Ipsum proin quis tortora maxima Aenean lobortis","KJ65","HF32"],
         "h":["2","3","Fer","Laculis euis mod In hac habitasse platea dictumus. Etiam dictum","HF32","ZX12"],
-        
     }
 
     print('drawing diagram...')
