26e226f55040f3fd09f29bac03a13d2b6f216c9e
DurandSacha <sacha6623@gmail.com>
2020-10-27 23:48:12 +0100

    clean template

--- app/utils.py
+++ app/utils.py
@@ -1,35 +1,12 @@
 import random
 import xlrd
-import numpy as np
-#import turtle
+
 from random import randint
 
-#TODO:  create a function for save excel document ( https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/ )
-
-def readExcelDocument():
-    workbook = xlrd.open_workbook('read_excel_file_with_python.xlsx')
+def readExcelDocument(file):
+    workbook = xlrd.open_workbook(file)
     worksheet = workbook.sheet_by_name(SheetNameList[0])
     print( 'num_rows, num_cells', worksheet.nrows, worksheet.ncols )
 
-
-
-"""
-def draw_random_line():
-    turtle.penup()
-    turtle.goto(-200,-200)
-    turtle.setheading(45)
-    turtle.pendown()
-
-    while True : 
-        # une couleur, un angle et une distance aléatoires...
-        rgb_r = randint(0,255) 
-        rgb_g = randint(0,255) 
-        rgb_b = randint(0,255) 
-        angle = randint(-10,10)
-        nbpas = randint(1,10) 
-        # ... pour comamnder la tortue
-        turtle.color(rgb_r,rgb_g, rgb_b)
-        turtle.left(angle)
-        turtle.forward(nbpas)
-"""
-
+def drawDiagram():
+    print('drawing diagram...')
--- app/views.py
+++ app/views.py
@@ -1,38 +1,21 @@
-from flask import Flask
-from flask import Flask, render_template
-from flask import Flask, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 
-#from .utils import find_content
+from .utils import readExcelDocument, drawDiagram
 
 app = Flask(__name__)
-
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
-#@app.route('/index/') ( définir plusieurs routes pour une vue )
 @app.route('/')
 def index():
+    drawDiagram()
     return render_template('index.html')
 
-@app.route('/result/')
-def result():
-    #param = request.args.get('param')
-    return render_template('result.html',
-                            #param=param,
-                            user_name='Sacha',
-                            user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'))
+@app.errorhandler(405)
+def method_not_allowed(e):
+    msg = f"The requested method '{request.method}' is not allowed."
+    return jsonify(error=msg), 405
 
-@app.route('/result/<result_id>/')
-def resultId():
-    #result_id = request.args.get('result_id')
-    return render_template('result.html',
-                            #result_id=result_id,
-                            user_name='Sacha',
-                            user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'))
-    # or return '%s' % content_id
-    # use utils.py function : description = find_content(gender).descripti
-
-
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
