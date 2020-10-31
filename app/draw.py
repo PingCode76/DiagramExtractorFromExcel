c73fef836791e873312b403bc669faa4059c3505
Yohann76 <yohanndurand76@gmail.com>
2020-10-31 16:49:48 +0100

    [Draw] update draw.py (filling)

--- app/draw.py
+++ app/draw.py
@@ -4,10 +4,10 @@ def drawDiagram():
 
     # Data in diagram
     nbRectangleInDiagram = countRectangleSection(data) #tab
-    nbLabel = [1]
+    nbLabelInRectangle = [3] #count for nb label in Rectangle
+    
 
     x = -1000
-
     # foreach tab 
     for rectangle in nbRectangleInDiagram:
         #print(nbRectangleInDiagram) # 3 
@@ -15,6 +15,10 @@ def drawDiagram():
         turtle.up()
         turtle.goto(x, 400)
         turtle.down()
+
+        turtle.fillcolor('grey') # background color 
+        turtle.begin_fill() # begin color 
+
         turtle.forward(300) #Forward turtle by 150 units ( width )
         turtle.left(90) #Turn turtle by 90 degree
         turtle.forward(600) #Forward turtle by 80 units ( height )
@@ -23,18 +27,24 @@ def drawDiagram():
         turtle.left(90) #Turn turtle by 90 degree
         turtle.forward(600) #Forward turtle by 80 units ( height )
         turtle.left(90) #Turn turtle by 90 degree
+
+        turtle.end_fill() # end color 
 
         x = x + 400
 
-        print(nbLabel)
         # for each rectangle, label
         t = -900 #x
         v = 800 #y
+
         for label in rectangle:
-            turtle.color('black')
+            turtle.color('green') #color 
             turtle.up()
-            turtle.goto(t,v)
+            turtle.goto(t,v) # pos
             turtle.down()
+
+            turtle.fillcolor('white') # background color 
+            turtle.begin_fill() # begin color 
+
             turtle.forward(150) #Forward turtle by 150 units ( width )
             turtle.left(90) #Turn turtle by 90 degree
             turtle.forward(100) #Forward turtle by 80 units ( height )
@@ -43,6 +53,8 @@ def drawDiagram():
             turtle.left(90) #Turn turtle by 90 degree
             turtle.forward(100) #Forward turtle by 80 units ( height )
             turtle.left(90) #Turn turtle by 90 degree
+
+            turtle.end_fill() # end color 
             turtle.up()
             print("NbLabel")
 
@@ -50,7 +62,6 @@ def drawDiagram():
             v = v - 150#y
 
             # Todo : add node 
-
             # first label 
 
             n = -900#x
@@ -60,17 +71,27 @@ def drawDiagram():
                 #node left
                 turtle.goto(n,m)
                 turtle.down()
-                turtle.color('blue')
+
+                turtle.color('blue') # color 
+                turtle.fillcolor('blue')
+                
+                turtle.begin_fill()
                 turtle.circle(25) #50 good 
-                #turtle.circle(50, 180)  #Trace un demi-cercle de rayon 70px
+                turtle.end_fill() 
+                
                 turtle.up()
 
                 #node right 
                 turtle.goto(n+150,m) # n + width + rayon 
                 turtle.down()
+
                 turtle.color('blue')
+                turtle.fillcolor('blue')
+
+                turtle.begin_fill()
                 turtle.circle(25) #50 good 
-                #turtle.circle(50, 180)  #Trace un demi-cercle de rayon 70px
+                turtle.end_fill() 
+
                 turtle.up()
 
                 m = m - 150 #y
