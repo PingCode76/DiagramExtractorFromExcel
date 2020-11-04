dc269e4f988adfc380dca6b3abb90db7a6c5ca1c
Yohann76 <yohanndurand76@gmail.com>
2020-11-04 22:51:51 +0100

    [draw] update label links

--- app/draw.py
+++ app/draw.py
@@ -1,5 +1,4 @@
 from .diagram import * 
-
 def drawingRectangle(nbDraw):
     x = -1000
     for rectangle in LabelInformation(nbDraw):
@@ -7,10 +6,8 @@ def drawingRectangle(nbDraw):
         turtle.up()
         turtle.goto(x, 400)
         turtle.down()
-
         #turtle.fillcolor('grey') # background color 
         #turtle.begin_fill() # begin color 
-
         turtle.forward(350) # ( width )
         turtle.left(90) # 90 degree
         turtle.forward(600) # ( height )
@@ -19,20 +16,15 @@ def drawingRectangle(nbDraw):
         turtle.left(90) # 90 degree
         turtle.forward(600) # ( height )
         turtle.left(90) # 90 degree
-
         #turtle.end_fill() # end color  
         x = x + 400
-
-
 def drawingLabel(x,y, txtLabel, colorLabel, txtNode1, txtNode2):  #y = 800 at starting
-
     turtle.color('green') #color 
     turtle.up()
     turtle.goto(x,y) # pos
     turtle.down()
     turtle.fillcolor('white') # background color 
     turtle.begin_fill() # begin color 
-
     turtle.forward(250) # ( width )
     turtle.left(90) # 90 degree
     turtle.forward(150) # ( height )
@@ -41,10 +33,8 @@ def drawingLabel(x,y, txtLabel, colorLabel, txtNode1, txtNode2):  #y = 800 at st
     turtle.left(90) # 90 degree
     turtle.forward(150) # ( height )
     turtle.left(90) # 90 degree
-
     turtle.end_fill() # end color 
     turtle.up()
-   
     y = y + 45 
     ## NODE
     # NODE LEFT
@@ -55,13 +45,10 @@ def drawingLabel(x,y, txtLabel, colorLabel, txtNode1, txtNode2):  #y = 800 at st
     turtle.begin_fill()
     turtle.circle(25)  
     turtle.end_fill() 
-
     turtle.goto(x-15,y+17) #go to for write
     turtle.color('white') # color txt
     turtle.write(txtNode1)
-
     turtle.up()
-
     # NODE RIGHT
     turtle.goto(x+250,y) # x + width + rayon 
     turtle.down()
@@ -70,31 +57,23 @@ def drawingLabel(x,y, txtLabel, colorLabel, txtNode1, txtNode2):  #y = 800 at st
     turtle.begin_fill()
     turtle.circle(25) 
     turtle.end_fill() 
-
     turtle.up()
     turtle.goto(x+235,y+13) #go to for write # test
     turtle.down()
     turtle.color('white') # color txt
     turtle.write(txtNode2)
-
     turtle.up()
-    
     ## WRITE IN "RECTANGLE FOR WRITE"
     x = x + 40 
-
     turtle.color('black') #color 
     turtle.up()
     turtle.goto(x,y) # pos
     turtle.down()
-
-
     turtle.write(txtLabel[0:27]) # 15 first character
     turtle.up()
-
     ## COLOR
     x = x - 10  #x 840 -> 860
     y = y + 45 #y #825
-
     turtle.color('black') #color 
     turtle.up()
     turtle.goto(x,y) # pos
@@ -109,13 +88,10 @@ def drawingLabel(x,y, txtLabel, colorLabel, txtNode1, txtNode2):  #y = 800 at st
     turtle.left(90) # 90 degree
     turtle.forward(10) # ( height )
     turtle.left(90) # 90 degree
-
     turtle.end_fill() # end color 
     turtle.up()
-
     ##  RECTANGLE FOR WRITE
     y = y - 60 
-
     turtle.color('black') #color 
     turtle.up()
     turtle.goto(x,y) # pos
@@ -129,111 +105,90 @@ def drawingLabel(x,y, txtLabel, colorLabel, txtNode1, txtNode2):  #y = 800 at st
     turtle.forward(50) # ( height )
     turtle.left(90) # 90 degree
     turtle.up()
-
-def drawingNode(xDraw,y,i,nbDraw,nbLabel):
-    
+def drawingNode(xDraw,y,i,nbDraw,VariablespositionLabels,nbLabel):
     # Import nodes
     nodes = nodeInformation(nbDraw) #output : {node1: [label1, label5], node2:... }
     LabelInfo = LabelInformation(nbDraw)
+    print(VariablespositionLabels)
 
     # for each nodes
-    for node in nodes:
-
+    for node in nodes: #
         # out node = i ( label number)
-        print("nodes")
         print(node)
-        print("nodes[node]")
-        print(nodes[node])
-        print("nodes entré")
-        print(nodes[node][0])
-        print("nodes sortie")
-        print(nodes[node][1])
-        print("i")
-        print(i)
+        # obtenir une position x
 
-
-        if nodes[node][1] == i : 
+        print("Label position x")
+        print(VariablespositionLabels['label'+str(i)+'x'])
     
-            print(y)
-            # face to face
-            if nodes[node][1] == i & nodes[node][0] == i or nbLabel == 1 :
-                print("face to face verified")
-                turtle.goto(xDraw+275,y+65)
-                turtle.down()
-                turtle.color('white') # color 
-                turtle.pensize(5.5)
-                turtle.forward(150) # ( width )
-                turtle.pensize(-5.5)
-                turtle.up()
- 
-            # top for bottom 
-            if y == 800 and nbLabel != 1 or nbLabel == 1 & nodes[node][0] == i  : 
-                print("top for bottom verified")
-                turtle.goto(xDraw+273,y+65) # y = 45 
-                turtle.down()
-                turtle.color('white') # color 
-                turtle.rt(59)
-                turtle.pensize(5.5)
-                turtle.forward(250) # ( width )
-                turtle.pensize(-5.5)
-                turtle.rt(-59) 
-                turtle.up()
+        # obtenir une position y
+        print("Label position y")
+        print(VariablespositionLabels['label'+str(i)+'y'])
+        
 
-            # bottom for top
-            if y == 600 and nbLabel != 1 :
-                print("bottom for top verified")
-                turtle.goto(xDraw+250,y+95)
-                turtle.down()
-                turtle.color('white') # color 
-                turtle.lt(53)
+
+        if nodes[node][1] == i: 
+            #while i > nbLabel:
+            if i > nbLabel:
+                # actual label 
+                print("position premier label")
+                labelFirstX = VariablespositionLabels['label'+str(nodes[node][0])+'x'] # pos x of in node
+                labelFirstY = VariablespositionLabels['label'+str(nodes[node][0])+'y'] # pos y of in node 
+                
+                # labelpremierY = VariablespositionLabels['label'+str(i)+'y'] # ? 
+
+                turtle.color('red') 
                 turtle.pensize(5.5)
-                turtle.forward(230) # ( width )
+                turtle.goto(labelFirstX + 272 , labelFirstY + 65) # pos start 
+                turtle.down()
                 turtle.pensize(-5.5)
-                turtle.lt(-53)
-            
-                turtle.up()
+ 
+                # pos finish label 
+                print("position deuxiéme label")
+                labelSecondX = VariablespositionLabels['label'+str(nodes[node][1])+'x'] # pos x of out node
+                labelSecondY = VariablespositionLabels['label'+str(nodes[node][1])+'y'] # pos y of out node
+                
+                turtle.color('green') 
+                turtle.pensize(5.5)
+                turtle.goto(labelSecondX,labelSecondY) # pos finish
+                #turtle.goto(30,30) # test
+                turtle.pensize(-5.5)
     return
 
 
 def drawDiagram(nbDraw):
-
     labelGroup = LabelInformation(nbDraw)
-
     xDraw = -1350
+    VariablespositionLabels = {}
     for i in labelGroup:
         #print(labelGroup[i])
-        
         # for each rectangle
         drawingRectangle(nbDraw)
-        # change pos draw
         xDraw = xDraw + 400
-        # count how label
         y = 600
         nbLabel = len(labelGroup[i])
         for i in labelGroup[i]:
             # get txt and node of label
+            # add y and y data in labelVariableXY
+
+            textnbLabel = "label" + str(i)
+            print(textnbLabel)
+            #VariablespositionLabels.update = ({textnbLabel + 'x': xDraw, textnbLabel + 'y': y})
+            VariablespositionLabels[textnbLabel + 'x'] = xDraw
+            VariablespositionLabels[textnbLabel + 'y'] = y
+
             line = selectLineinData(int(i), nbDraw)
-
-
             #print(line)
             txtLabel = line[3] # get txt in DataLine - inject to drawingLabel
             #print(txtLabel)
-
             # get color of label
             color = checkdifferentsColor(nbDraw)
             colorLabel = color[i] # get color - inject to drawingLabel
             #print(color[i])
-            
             # get txt node 1
             txtNode1 = line[4] # get txt node 1 - inject to drawingLabel
             # get txt node 
             txtNode2 = line[5] # get txt node 2 - inject to drawingLabel
-
-            if nbLabel == 1:
-                y = 800
-            drawingLabel(xDraw, y , txtLabel , colorLabel ,txtNode1, txtNode2 )  #inject here
-
-            drawingNode(xDraw,y,i,nbDraw,nbLabel) 
-            y = y + 200
             
-
+            drawingLabel(xDraw, y , txtLabel , colorLabel ,txtNode1, txtNode2 )  #inject here
+            drawingNode(xDraw,y,i,nbDraw, VariablespositionLabels,nbLabel) 
+            y = y + 200
